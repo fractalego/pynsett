16b84994b10beb83d9dfff3ce92ca6e22200d481
Alberto Cetoli <alberto@contextscout.com>
2017-10-03 18:04:06 +0100

    Added personal pronouns

--- pynsett/examples/sentence_creation.py
+++ pynsett/examples/sentence_creation.py
@@ -8,7 +8,7 @@ from pynsett.writer import RelationTripletsWriter
 
 _path = os.path.dirname(__file__)
 
-sentence = 'Jane\'s dog is red'
+sentence = 'My own dog is red'
 #sentence = 'Jane has a bicycle'
 #sentence = 'Jane is an engineer'
 #sentence = 'Jane works as an engineer'

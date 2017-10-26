3d8ee5117e82dd7ed3fda3008632a4e4eae40065
Alberto Cetoli <alberto@contextscout.com>
2017-10-26 16:45:35 +0100

    pip version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pynsett',
-      version='0.0.1',
+      version='0.0.2',
       description='A relation extractor',
       url='http://github.com/fractalego/pynsett',
       author='Alberto Cetoli',

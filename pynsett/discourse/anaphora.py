a0e16b44102df2a5d97f5e91f57d4d0339b5d9c3
alberto cetoli <alberto.cetoli@nlulite.com>
2017-11-08 23:27:12 +0000

    introducing anaphora rules

--- pynsett/discourse/anaphora.py
+++ pynsett/discourse/anaphora.py
@@ -1,3 +1,17 @@
+import logging
+import os
+
+from parvusdb import GraphDatabase
+
+_path = os.path.dirname(__file__)
+
+
 class Anaphora:
+    __logger = logging.getLogger(__name__)
+    _db = GraphDatabase(g)
+
     def __init__(self):
+        self._rules = open(os.path.join(_path, '../rules/anaphora.parvus')).read()
+
+    def apply_on_one_drs(self, drs):
         pass

70fc7fd58224b4257425d1b99848325f02dd97e0
Alberto Cetoli <alberto@contextscout.com>
2017-11-14 18:42:53 +0000

    Added InterSentenceAnaphoraVisitor

--- pynsett/discourse/anaphora.py
+++ pynsett/discourse/anaphora.py
@@ -11,7 +11,21 @@ class SingleSentenceAnaphoraVisitor:
     __logger = logging.getLogger(__name__)
 
     def __init__(self):
-        self._rules = open(os.path.join(_path, '../rules/anaphora.parvus')).read()
+        self._rules = open(os.path.join(_path, '../rules/intra_sentence_anaphora.parvus')).read()
+
+    def apply(self, g):
+        if not isinstance(g, Graph):
+            raise TypeError("DrsRule.apply_to_graph() needs an igraph.Graph as an argument")
+        db = GraphDatabase(g)
+        lst = db.query(self._rules, repeat_n_times=1)
+        return lst
+
+
+class InterSentenceAnaphoraVisitor:
+    __logger = logging.getLogger(__name__)
+
+    def __init__(self):
+        self._rules = open(os.path.join(_path, '../rules/inter_sentence_anaphora.parvus')).read()
 
     def apply(self, g):
         if not isinstance(g, Graph):

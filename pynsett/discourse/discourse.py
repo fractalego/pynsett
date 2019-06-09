15a481b8d0e91966a02a71c784085a6885f4fa9b
alberto cetoli <alberto.cetoli@nlulite.com>
2019-06-09 14:54:56 +0100

    Added DiscourseBase

--- pynsett/discourse/discourse.py
+++ pynsett/discourse/discourse.py
@@ -12,7 +12,66 @@ from ..drt import Drs
 _logger = logging.getLogger(__name__)
 
 
-class Discourse:
+class DiscourseBase:
+    _sentences_list = []
+    _drs_list = []
+    _discourse = Drs.create_empty()
+
+    @property
+    def drs_list(self):
+        return self._drs_list
+
+    @property
+    def connected_components(self):
+        from igraph import WEAK
+        g_list = self._discourse._g.clusters(mode=WEAK).subgraphs()
+        return [Drs(g) for g in g_list]
+
+    def __getitem__(self, item):
+        return self._sentences_list[item], self._drs_list[item]
+
+    def __len__(self):
+        return len(self._sentences_list)
+
+    def get_discourse_drs(self):
+        return self._discourse
+
+
+class Paragraph(DiscourseBase):
+    def __init__(self, text):
+        self._discourse = Drs.create_empty()
+        self._drs_list = []
+
+        self._sentences_list = sent_tokenize(text)
+        word_nodes = []
+        for sentence_index, sentence in enumerate(self._sentences_list):
+            try:
+                if sentence == '.':
+                    continue
+                drs = Drs.create_from_natural_language(sentence)
+                word_nodes += assign_proper_index_to_nodes_names(drs.word_nodes, sentence_index)
+                drs.apply(HeadTokenVisitor(sentence_index))
+                drs.apply(SentenceNamesModifier(sentence_index))
+                self._drs_list.append(drs)
+            except Exception as e:
+                _logger.warning('Exception caught in Discourse: ' + str(e))
+
+        coreference_visitor_factory = AllenCoreferenceVisitorsFactory(word_nodes)
+        for i, drs in enumerate(self._drs_list):
+            drs.apply(coreference_visitor_factory.create())
+
+        self.__create_discourse_graph()
+
+    def __create_discourse_graph(self):
+        if len(self._drs_list) == 1:
+            self._discourse = self._drs_list[0]
+            return
+        for drs in self._drs_list:
+            self._discourse.apply(GraphJoinerVisitor(drs))
+        self._discourse.apply(CoreferenceJoinerVisitor())
+
+
+class Discourse(DiscourseBase):
     def __init__(self, text):
         self.paragraphs = self.__divide_into_paragraphs(self.__sanitize_text(text))
         self._sentences_list = self.__aggregate_sentence_list_from_paragrahs(self.paragraphs)
@@ -48,77 +107,3 @@ class Discourse:
         text = text.replace('..', '.')
         text = text.replace('\n.', '\n')
         return text
-
-    @property
-    def drs_list(self):
-        return self._drs_list
-
-    @property
-    def connected_components(self):
-        from igraph import WEAK
-
-        g_list = self._discourse._g.clusters(mode=WEAK).subgraphs()
-        return [Drs(g) for g in g_list]
-
-    def __getitem__(self, item):
-        return self._sentences_list[item], self._drs_list[item]
-
-    def __len__(self):
-        return len(self._sentences_list)
-
-    def get_discourse_drs(self):
-        return self._discourse
-
-
-class Paragraph:
-    def __init__(self, text):
-        self._discourse = Drs.create_empty()
-        self._drs_list = []
-
-        self._sentences_list = sent_tokenize(text)
-        word_nodes = []
-        for sentence_index, sentence in enumerate(self._sentences_list):
-            try:
-                if sentence == '.':
-                    continue
-                drs = Drs.create_from_natural_language(sentence)
-                word_nodes += assign_proper_index_to_nodes_names(drs.word_nodes, sentence_index)
-                drs.apply(HeadTokenVisitor(sentence_index))
-                drs.apply(SentenceNamesModifier(sentence_index))
-                self._drs_list.append(drs)
-            except Exception as e:
-                self._logger.warning('Exception caught in Discourse: ' + str(e))
-
-        coreference_visitor_factory = AllenCoreferenceVisitorsFactory(word_nodes)
-        for i, drs in enumerate(self._drs_list):
-            drs.apply(coreference_visitor_factory.create())
-
-        self.__create_discourse_graph()
-
-    @property
-    def drs_list(self):
-        return self._drs_list
-
-    @property
-    def connected_components(self):
-        from igraph import WEAK
-
-        g_list = self._discourse._g.clusters(mode=WEAK).subgraphs()
-        return [Drs(g) for g in g_list]
-
-    def __create_discourse_graph(self):
-        if len(self._drs_list) == 1:
-            self._discourse = self._drs_list[0]
-            return
-        for drs in self._drs_list:
-            self._discourse.apply(GraphJoinerVisitor(drs))
-        self._discourse.apply(CoreferenceJoinerVisitor())
-
-    def __getitem__(self, item):
-        return self._sentences_list[item], self._drs_list[item]
-
-    def __len__(self):
-        return len(self._sentences_list)
-
-    def get_discourse_drs(self):
-        return self._discourse

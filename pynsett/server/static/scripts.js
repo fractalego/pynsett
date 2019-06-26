d716b64e6930bb7655f14f1e472dfd7f3a98406a
alberto cetoli <alberto.cetoli@nlulite.com>
2019-06-26 22:22:59 +0100

    simple web server for drt

--- pynsett/server/static/scripts.js
+++ pynsett/server/static/scripts.js
@@ -8,6 +8,5 @@ function drt_triplets_from_api(cb){
         err = '';
         cb(data, err);
     }
-    request.send(JSON.stringify({'text': 'Isaac Asimov was an American writer and professor of biochemistry at Boston University. He was known for his works of science fiction and popular science. Asimov was a prolific writer who wrote or edited more than 500 books and an estimated 90,000 letters and postcards. His books have been published in 9 of the 10 major categories of the Dewey Decimal Classification.'}));
-
+    request.send(JSON.stringify({'text': document.getElementById('query').value}));
 }

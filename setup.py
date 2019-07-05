abe41d5f0803c6e5c059b11236ccbbf80ff64efd
alberto cetoli <alberto.cetoli@nlulite.com>
2019-07-05 19:55:15 +0100

    changed version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pynsett',
-      version='0.1.0',
+      version='0.1.2',
       description='A relation extractor',
       url='http://github.com/fractalego/pynsett',
       author='Alberto Cetoli',

403c952ebb4f5b2606c933907d2c97925e47c61a
alberto cetoli <alberto.cetoli@nlulite.com>
2019-07-05 23:31:49 +0100

    Added the files in data/

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pynsett',
-      version='0.1.2',
+      version='0.1.3',
       description='A relation extractor',
       url='http://github.com/fractalego/pynsett',
       author='Alberto Cetoli',
